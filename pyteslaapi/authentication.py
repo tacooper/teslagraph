daa724c333a1f29f8e221a0cfe6f07999d3f5243
Tom Cooper <tacooper@vt.edu>
2019-10-14 20:21:45 -0400

    Refactored authentication for sending requests.

--- pyteslaapi/authentication.py
+++ pyteslaapi/authentication.py
@@ -9,37 +9,45 @@ CLIENT_SECRET = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa
 
 # uses account email/password or refresh token to obtain access token for authenticating API calls
 class Authentication:
-    def __init__(self, email, password, refresh_token=None):
-        # build request with email/password or refresh token
-        if refresh_token is None:
-            request_data = {
-                "grant_type": "password",
-                "client_id": CLIENT_ID,
-                "client_secret": CLIENT_SECRET,
-                "email": email,
-                "password": password
-            }
-        else:
-            request_data = {
-                "grant_type": "refresh_token",
-                "client_id": CLIENT_ID,
-                "client_secret": CLIENT_SECRET,
-                "refresh_token": refresh_token
-            }
+    def __init__(self):
+        self.refresh_token = None
+        self.headers = None
 
+    def send_initial_request(self, email, password):
+        # build request with email/password
+        request_data = {
+            "grant_type": "password",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "email": email,
+            "password": password
+        }
+
+        self.send_request(request_data)
+
+    def send_refresh_request(self):
+        # build request with refresh token
+        request_data = {
+            "grant_type": "refresh_token",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "refresh_token": self.refresh_token
+        }
+
+        self.send_request(request_data)
+
+    def send_request(self, request_data):
         # send request to get response with access token
         response = requests.post("https://owner-api.teslamotors.com/oauth/token", data=request_data).json()
-        if "response" in response:
-            # handle response for invalid request
-            print(response["response"])
-            sys.exit(1)
-        elif "error_description" in response:
+        if "error_description" in response:
             # handle response for invalid request
             print(response["error_description"])
-            sys.exit(1)
-
-        # store refresh token and authorization headers for API calls
-        self.refresh_token = response["refresh_token"]
-        self.headers = {
-            "Authorization": "Bearer " + response["access_token"]
-        }
+        elif "response" in response:
+            # handle response for invalid request
+            print(response["response"])
+        else:
+            # store refresh token and authorization headers for API calls
+            self.refresh_token = response["refresh_token"]
+            self.headers = {
+                "Authorization": "Bearer " + response["access_token"]
+            }
